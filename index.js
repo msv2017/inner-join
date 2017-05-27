d61f73ab0eea666040f1676f041e197a1adf2517
msv2017 <sergm2005@gmail.com>
2017-05-27 18:38:05 +0300

    4

--- index.js
+++ index.js
@@ -3,21 +3,29 @@ module.exports = function () {
 	var args = Array.prototype.slice.call(arguments, 0, -1);
 	var fn = Array.prototype.slice.call(arguments, -1)[0];
 
+	var re = /(\w+)\s*=>\s*\1+\.(\w+)/g;
+	var joinKey = re.exec(fn.toString())[2];
+	
 	[].concat.apply([], args).forEach(x => { var key = fn(x); var t = cache[key] || []; t.push(x); cache[key] = t; });
 
-	if (cache.hasOwnProperty(true))
-		return cache[true];
-
+	var list = [];
 	var keys = Object.keys(cache);
-	var maxKey = null;
-	var max = 0;
 	for (var i = 0; i < keys.length; i++) {
 		var key = keys[i];
-		if (cache[key].length > max) {
-			max = cache[key].length;
-			maxKey = key;
+		var t = {};
+		t[joinKey] = key;
+		for(var j = 0; j < cache[key].length; j++){
+			var o = cache[key][j];
+			var keys2 = Object.keys(o);
+			for(var k = 0; k < keys2.length; k++){
+				if(keys2[k] !== joinKey){
+					t[keys2[k]] = o[keys2[k]];
+				}
+			}
 		}
+		
+		list.push(t);
 	}
 
-	return cache[maxKey];
+	return list;
 }
